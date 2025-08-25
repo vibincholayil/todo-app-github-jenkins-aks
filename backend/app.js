e3597afa4a685c3a1f1eace1fc9d3494b3928c15
PrinceShawtz <129332944+PrinceShawtz@users.noreply.github.com>
2025-08-25 11:38:11 -0400

    Created app.js

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const todoRoutes = require('./routes/todos');
+
+app.use(express.json());
+app.use('/api/todos', todoRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

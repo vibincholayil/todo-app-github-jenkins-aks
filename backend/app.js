d5667a1fdf2b62254bad3ee7c45c68a120a58c51
Vibin Nath Cholayil <vibinnath.c@gmail.com>
2025-08-27 17:04:45 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const todoRoutes = require('./routes/todos');
+const todoRoutes = require('./routes/todoRoutes');
 
 app.use(express.json());
 app.use('/api/todos', todoRoutes);

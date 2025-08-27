15ad6717cd308e865e1286fd2d24c5bb82ddab74
Vibin Nath Cholayil <vibinnath.c@gmail.com>
2025-08-27 17:06:50 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ const todoRoutes = require('./routes/todos');
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Todo App API! Use /api/todos to access todos.');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
